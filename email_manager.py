83cc350613f7113337d5aca3f8d0fee2990abdfc
Samuel <ogboyesam@gmail.com>
2023-07-30 10:26:01 -0700

    email sending implemented

---
+++ email_manager.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+
+import csv
+import smtplib
+import ssl
+
+message = """Hi {fname},
+I wish you a very Happy Birthday.
+I hope you had a great day."""
+
+sender_address = "samplecoder2023@gmail.com"
+sender_password = "wkqwcbsfwevdydxg"
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    server.login(sender_address, sender_password)
+
+    with open(r"birthday.csv") as birthdays:
+        reader = csv.reader(birthdays)
+        next(reader)
+        for fname, lname, email, dob in reader:
+            server.sendmail(
+                sender_address,
+                email,
+                message.format(fname=fname),
+            )
+
+choice = input(
+    """Do you wish to add or remove names from the csv file?
+    if you would like to add names type add
+    otherwise if you would like to remove names type remove
+    When you are finished, type exit
+    """
+)
+
+if choice == "add":
+    new_data = input("Enter data as first name,lastname,email,date of birth: ")
+    new_data = new_data.split(",")
+
+    with open("birthday.csv", "r", newline="") as file:
+        reader = csv.reader(file)
+        existing_data = list(reader)
+
+    with open("birthday.csv", "w", newline="") as file:
+        writer_object = csv.writer(file)
+        writer_object.writerows(existing_data)
+        writer_object.writerow(new_data)
+
+#    with open(r"birthday.csv", "a") as file:
+#        writer_object = csv.writer(file)
+#        next(file)
+#        writer_object.writerow(new_data)
+elif choice == "remove":
+    lines = []
+    removal = input("Enter the first name of the person to be removed: ")
+    with open(r"birthday.csv", "r") as file:
+        reader = csv.reader(file)
+
+        for row in reader:
+            lines.append(row)
+
+        for fields in row:
+            if fields == removal:
+                lines.remove(row)
+
+    with open(r"birthday.csv", "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerows(lines)
